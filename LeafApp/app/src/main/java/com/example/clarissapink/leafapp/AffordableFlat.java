007172ebcb63ddf8cc6092d9bca3c0afb57dbad7
Limeye95 <emilychin95@hotmail.com>
2016-03-08 17:05:06 +0800

    Added affordableflatresult file, updated spinner for affordableflat

--- LeafApp/app/src/main/java/com/example/clarissapink/leafapp/AffordableFlat.java
+++ LeafApp/app/src/main/java/com/example/clarissapink/leafapp/AffordableFlat.java
@@ -3,8 +3,15 @@ package com.example.clarissapink.leafapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AffordableFlat extends AppCompatActivity {
+    Spinner spinner;
+    ArrayAdapter<CharSequence> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +19,23 @@ public class AffordableFlat extends AppCompatActivity {
         setContentView(R.layout.activity_affordable_flat);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
 
+        spinner = (Spinner) findViewById(R.id.spinnerAFRepaymentPeriod);
+        adapter = ArrayAdapter.createFromResource(this, R.array.repaymentPeriod, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getBaseContext(), parent.getItemIdAtPosition(position) + " sel   ected", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+
+    }
 }
